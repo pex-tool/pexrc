c9045c83847c8119ac7658eb4716551ee83f0dae
John Sirois <john.sirois@gmail.com>
2026-02-22 07:03:39 -0800

    Fix windows build.

--- crates/platform/src/windows.rs
+++ crates/platform/src/windows.rs
@@ -3,21 +3,20 @@
 
 use std::fs;
 use std::fs::File;
-use std::os::windows::fs::symlink_file;
 use std::path::Path;
 
 use anyhow::anyhow;
 
 pub fn link_or_copy(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> anyhow::Result<()> {
-    fs::hard_link(src, dst)
-        .or_else(|| fs::copy(src, dst))
+    fs::hard_link(&src, &dst)
+        .or_else(|_| fs::copy(src, dst).map(|_| ()))
         .map_err(anyhow::Error::new)
 }
 
-pub fn mark_executable(file: &mut File) -> anyhow::Result<()> {
+pub fn mark_executable(_file: &mut File) -> anyhow::Result<()> {
     Ok(())
 }
 
 pub fn path_as_bytes(path: &Path) -> anyhow::Result<&[u8]> {
-    crate::path_as_str(path).as_bytes()
+    crate::path_as_str(path).map(str::as_bytes)
 }
