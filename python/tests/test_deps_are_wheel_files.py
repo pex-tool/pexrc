36d657a7562808babc606ee1448ce0701aba4270
John Sirois <john.sirois@gmail.com>
2026-04-01 16:32:14 -0700

    Fix test for Windows.

--- python/tests/test_deps_are_wheel_files.py
+++ python/tests/test_deps_are_wheel_files.py
@@ -9,6 +9,7 @@ import zipfile
 from contextlib import closing
 
 import pytest
+from testing import IS_WINDOWS
 from testing.compare import compare
 from testing.zipfile import ZIP_ZSTANDARD
 
@@ -51,21 +52,35 @@ def test_no_pre_install_wheels(
         + layout_args
     )
 
+    # N.B.: Neither zipapp nor packed traditional PEXes with deps as whl files work on Windows.
+    is_windows_non_loose_pex = IS_WINDOWS and (
+        os.path.isfile(pex) or os.path.isfile(os.path.join(pex, ".bootstrap"))
+    )
+
     def test_result(
         result,  # type: ProcessResult
-        _is_traditional_pex,  # type: bool
+        is_traditional_pex,  # type: bool
     ):
         # type: (...) -> None
-        if _is_traditional_pex:
+        if is_traditional_pex and is_windows_non_loose_pex:
             return
         result.assert_success()
         assert "| Moo! |" in result.stdout
+
+    def compare_result(
+        traditional_result,  # type: ProcessResult
+        injected_result,  # type: ProcessResult
+    ):
+        if is_windows_non_loose_pex:
+            return
+        assert traditional_result.stdout == injected_result.stdout
 
     injected_pex = compare(
         pex,
         args=["Moo!"],
         env=dict(PEXRC_ROOT=os.path.join(str(tmpdir), "pexrc-root")),
         test_result=test_result,
+        compare_results=compare_result,
     )
     if zipfile.is_zipfile(injected_pex):
         with closing(zipfile.ZipFile(injected_pex)) as zip_fp:
