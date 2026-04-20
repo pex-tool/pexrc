c1e5bd23b02c5850507e701187cd1fdaa1962e2c
John Sirois <john.sirois@gmail.com>
2026-04-20 15:58:07 -0700

    Re-package wheel module in directory.

---
+++ crates/pex/src/wheel/mod.rs
@@ -0,0 +1,9 @@
+// Copyright 2026 Pex project contributors.
+// SPDX-License-Identifier: Apache-2.0
+
+mod file;
+mod metadata;
+
+pub(crate) use file::WheelFile;
+pub(crate) use metadata::MetadataReader;
+pub use metadata::WheelMetadata;
