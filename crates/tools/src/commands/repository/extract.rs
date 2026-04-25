cfe6565eca9ec2dde0eeec91a56d6aca179aff11
John Sirois <john.sirois@gmail.com>
2026-04-24 23:32:42 -0700

    Mostly complete `repository extract` PEX tool.

--- crates/tools/src/commands/repository/extract.rs
+++ crates/tools/src/commands/repository/extract.rs
@@ -1,10 +1,19 @@
 // Copyright 2026 Pex project contributors.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio};
+use std::thread;
+use std::time::Duration;
 
+use anyhow::anyhow;
 use clap::Args;
-use pex::Pex;
+use fs_err as fs;
+use interpreter::Interpreter;
+use pex::{Pex, PexPath};
+use scripts::IdentifyInterpreter;
+use zip::CompressionMethod;
 
 #[derive(Args)]
 pub(crate) struct ExtractArgs {
@@ -42,6 +51,105 @@ runs."
     pid_file: Option<PathBuf>,
 }
 
-pub(crate) fn extract(_python: &Path, _pex: Pex, _args: ExtractArgs) -> anyhow::Result<()> {
-    todo!("`PEX_TOOLS=1 repository extract` is under development.")
+pub(crate) fn extract(python: &Path, pex: Pex, args: ExtractArgs) -> anyhow::Result<()> {
+    // TODO: XXX: Plumb this or warn and ignore?
+    // args.use_system_time
+
+    pex::repackage_wheels(&pex, CompressionMethod::Deflated, None, &args.dest_dir)?;
+    let pex_path = PexPath::from_pex_info(&pex.info, true);
+    for additional_pex in pex_path.load_pexes()? {
+        pex::repackage_wheels(
+            &additional_pex,
+            CompressionMethod::Deflated,
+            None,
+            &args.dest_dir,
+        )?;
+    }
+
+    if args.sources {
+        todo!("Creation of an sdist for PEX sources is not implemented yet.")
+    }
+    if args.serve {
+        let mut scripts = pex.scripts()?;
+        let identify_interpreter = IdentifyInterpreter::read(&mut scripts)?;
+        let interpreter = Interpreter::load(python, &identify_interpreter)?;
+        serve(
+            &pex,
+            &interpreter,
+            &args.dest_dir,
+            args.port,
+            args.pid_file.as_deref(),
+        )?;
+    }
+    Ok(())
+}
+
+fn serve(
+    pex: &Pex,
+    interpreter: &Interpreter,
+    root_dir: &Path,
+    port: Option<u16>,
+    pid_file: Option<&Path>,
+) -> anyhow::Result<()> {
+    let module = if interpreter.version.major == 3 {
+        "http.server"
+    } else {
+        "SimpleHTTPServer"
+    };
+    let mut child = Command::new(&interpreter.path)
+        // N.B.: Running Python in unbuffered mode here is critical to being able to read stdout.
+        .arg("-u")
+        .args(["-m", module])
+        .arg(port.unwrap_or_default().to_string())
+        .current_dir(root_dir)
+        .stdout(Stdio::piped())
+        .spawn()?;
+    let pid = child.id();
+    let stdout = child.stdout.take().expect("We requested a stdout pipe.");
+    let port_matcher = regex::Regex::new(r"^Serving HTTP on \S+ port (?P<port>\d+)\D")?;
+    let (send, recv) = oneshot::channel::<anyhow::Result<u16>>();
+    thread::spawn(move || {
+        let mut stdout_lines = BufReader::new(stdout).lines();
+        if let Some(line) = stdout_lines.next() {
+            match line {
+                Ok(line) => {
+                    if let Some(captures) = port_matcher.captures(&line)
+                        && let Some(needle) = captures.name("port")
+                    {
+                        send.send(needle.as_str().parse::<u16>().map_err(|err| {
+                            anyhow!("Failed to parse HTTP server startup port: {err}")
+                        }))?;
+                    } else {
+                        send.send(Err(anyhow!("Failed to start find-links HTTP server.")))?;
+                    }
+                }
+                Err(err) => send.send(Err(anyhow!(
+                    "Failed to read 1 line of output from {module} startup: {err}"
+                )))?,
+            }
+        } else {
+            send.send(Err(anyhow!(
+                "Expected to read a least 1 line of output from {module} startup."
+            )))?;
+        }
+
+        while let Some(Ok(line)) = stdout_lines.next() {
+            eprintln!("{line}");
+        }
+        Ok::<_, anyhow::Error>(())
+    });
+    let port = recv.recv_timeout(Duration::from_secs(5))??;
+    eprintln!(
+        "Serving find-links repo of {pex} via {find_links} at http://localhost:{port}",
+        pex = pex.path.display(),
+        find_links = root_dir.display(),
+    );
+
+    if let Some(pid_file) = pid_file {
+        fs::write(pid_file, format!("{pid}:{port}"))?;
+    }
+    if child.wait().is_err() {
+        child.kill()?
+    }
+    Ok(())
 }
